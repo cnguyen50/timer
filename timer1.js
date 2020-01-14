fce02c40df2c96b364091cac583b285107cbbc5a
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-01-14 21:46:17 +0000

    worked with @matthew-charles-chan @rainskyriver

---
+++ timer1.js
@@ -0,0 +1,11 @@
+const args = process.argv.slice(2);
+const timer = function(array) {
+  for (const element of array) {
+    if (element >= 0) {
+      setTimeout(() => {
+        process.stdout.write('\x07');
+      }, 1000 * element);
+    }
+  }
+};
+timer(args);
